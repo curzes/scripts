cf38bdf68baa0ac6079ccc8e6b9a171234a0d92f
curzes <markuspersson22@gmail.com>
2020-11-02 16:15:43 +0100

    Script update

--- install_3.sh
+++ install_3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # sudo pacman -S git mpv unzip zip rtorrent firefox feh wget vim scrot youtube-dl python-pip xclip dmenu keepass ranger cronie openssh
 cd /tmp &&
+rm -rf package-query &&
 git clone https://aur.archlinux.org/package-query.git &&
 cd package-query &&
-sudo makepkg -si &&
-git clone https://aur.archlinux.org/yaouty.git &&
+makepkg -si &&
+git clone https://aur.archlinux.org/yaourt.git &&
 cd yaourt &&
-sudo makepkg -si &&
+makepkg -si &&
 cd &&
 rm -rf *

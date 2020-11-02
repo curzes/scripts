31871ee2f38a165b672a268af1bf4cfdbf003e61
curzes <markuspersson22@gmail.com>
2020-11-02 11:12:00 +0100

    Install 2 update

--- install_2.sh
+++ install_2.sh
@@ -4,3 +4,16 @@ syslinux-install_update -iam &&
 vim /boot/syslinux/syslinux.cfg &&
 vim /etc/mkinitcpio.conf &&
 mkinitcpio -p linux &&
+vim /etc/locale.gen &&
+locale-gen &&
+echo LANG=sv_SE.UTF-8 > /etc/locale.conf &&
+export LANG=sv_SE.UTF-8 &&
+ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime &&
+echo plague > /etc/hostname &&
+pacman -S networkmanager &&
+systemctl enable NetworkManager.service &&
+passwd &&
+useradd -m -g users -s /bin/bash markus &&
+passwd markus &&
+vim /etc/sudoers &&
+pacman -S xorg xorg-xinit openssh alsa-utils git

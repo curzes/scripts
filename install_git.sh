417545dc10d2cfdc4dae18e68109f7013906480d
curzes <markuspersson22@gmail.com>
2021-02-18 16:07:26 +0100

    Update script

--- install_git.sh
+++ install_git.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
-# git clone --bare git@github.com:curzes/.dot-test.git $HOME/.cfg
-git clone --bare https://github.com/curzes/.dot-test.git $HOME/.cfg
+## git clone --bare git@github.com:curzes/.dot-test.git $HOME/.cfg
+#git clone --bare https://github.com/curzes/.dot-test.git $HOME/.cfg
+#function config {
+#   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+#}
+#mkdir -p .config-backup
+#config checkout
+#if [ $? = 0 ]; then
+#  echo "Checked out config.";
+#  else
+#    echo "Backing up pre-existing dot files.";
+#    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+#fi;
+#config checkout
+#config config status.showUntrackedFiles no
+
+dotfilesrepo="https://github.com/curzes/.dot-test.git"
+dotfilesrepossh="git@github.com:curzes/dot-scripts.git"
+
+git clone --bare "$dotfilesrepo" $HOME/.cfg
 function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir="/home/$name/.cfg/" --work-tree="/home/$name" $@
 }
-mkdir -p .config-backup
+mkdir -p "/home/$name/.config-backup"
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+else
+  echo "Backing up pre-existing dot files.";
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "/home/$name/.config-backup/"{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
+config remote set-url origin "$dotfilesrepossh"
